81760683ad6ef4398396aebc31f36800fd089fea
karanphadnisnaik <Karanphadnis1@gmail.com>
2016-08-30 22:09:18 -0700

    added gifmesomething -- basic formatting needed

--- app/features/feature2/feature2.js
+++ app/features/feature2/feature2.js
@@ -11,6 +11,36 @@ angular
       })
     ;
   })
-  .controller('Feature2Ctrl', function () {
+  .controller('Feature2Ctrl', Feature2Ctrl);
 
-  });
+Feature2Ctrl.$inject = [
+  '$scope',
+  '$http',
+];
+
+
+function Feature2Ctrl ($scope, $http) {
+    //$scope.searchQuery = "randomthing";
+    $scope.stuff = 'http://i.giphy.com/GvUb9lnnnsjYY.gif';
+    $scope.submit = function() {
+        var query;
+
+        if (false) // search querry is not alpha numeric
+        {
+            // ask them to send an alpha numeric one
+        }
+
+        query = $scope.searchQuery.toLowerCase().split(' ').join('+');
+
+        $http
+        .get('http://api.giphy.com/v1/gifs/search?q=' + query + '&limit=1&api_key=dc6zaTOxFJmzC')
+        .then(function (response) {
+            console.log("Request was a success");
+            if(response.data.meta.msg === "OK") {
+                console.log(response.data.data[0].images.fixed_width_downsampled.url);
+                $scope.stuff = response.data.data[0].images.fixed_width_downsampled.url;
+            }
+        });
+    };
+
+}
